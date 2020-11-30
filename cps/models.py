9180c939cc7cda4744d59ac82881b4aa7f4be139
Hussain Mohamed <noor.xbyte@gmail.com>
2020-11-30 18:24:05 +0500

    added available field to genset

--- cps/models.py
+++ cps/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Genset(models.Model):
+    available = models.BooleanField(default=True)
     number = models.IntegerField(unique=True) 
     serial_number = models.CharField(max_length=200)
     make = models.CharField(max_length=200)
     model = models.CharField(max_length=200)
     cap_kw = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
