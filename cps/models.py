372f38bba5469590f3e8c8fba09acc23c28c1fc0
Hussain Mohamed <noor.xbyte@gmail.com>
2020-11-30 18:15:50 +0500

    created genset model seeder and admin

--- cps/models.py
+++ cps/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Genset(models.Model):
+    number = models.IntegerField(unique=True) 
+    serial_number = models.CharField(max_length=200)
+    make = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    cap_kw = models.IntegerField()
