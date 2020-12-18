c918e886d64fe53648a27270baf0a18f6c93e8e5
Hussain Mohamed <noor.xbyte@gmail.com>
2020-12-18 19:06:58 +0500

    DELETED ALL MODELS AND STARTING AGAIN

--- cps/admin.py
+++ cps/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from .models import Genset, RunRecord, DayTankInfo
-
 # Register your models here.
-
-admin.site.register(Genset)
-admin.site.register(RunRecord)
-admin.site.register(DayTankInfo)
